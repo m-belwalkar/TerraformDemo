b6ec99dec13284ce71587f4fee38a65b24bc5333
Mangesh Belwalkar <belwalkarmangesh@gmail.com>
2022-01-27 20:20:53 +0530

    Added backend

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  
+  backend "azure" {}
   required_version = ">=0.12"
   
   required_providers {
@@ -32,4 +32,19 @@ resource "azurerm_subnet" "example" {
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.7.29.0/29"]
   service_endpoints    = ["Microsoft.Sql"]
+}
+
+resource "azurerm_storage_account" "rgname" {
+  name                     = "cdcbackinginfrastorageaccount"
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access  = true
+
+}
+resource "azurerm_storage_container" "rgname" {
+  name                  = "cdccontainer"
+  storage_account_name  = azurerm_storage_account.rgname.name
+  container_access_type = "container"
 }

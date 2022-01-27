14b8076d906d2d726e420a0b255e50c59c3e8c98
Mangesh Belwalkar <belwalkarmangesh@gmail.com>
2022-01-27 18:08:08 +0530

    Inital commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  default       = "CDCDemoTerraform"
+  description   = "Name of the resource group."
+}
+
+variable "resource_group_location" {
+  default = "westus"
+  description   = "Location of the resource group."
+}

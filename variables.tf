0b1ceec24454c1d27119e841eb020ecfa3543e26
Mangesh Belwalkar <belwalkarmangesh@gmail.com>
2022-01-27 21:31:55 +0530

    Adding App service names

--- variables.tf
+++ variables.tf
@@ -7,3 +7,12 @@ variable "resource_group_location" {
   default = "westus"
   description   = "Location of the resource group."
 }
+
+variable "azurerm_app_service_plan_name" {
+  default = "cdcpocdemo1"
+  description   = "App Service plan name."
+}
+variable "azurerm_app_service_producer" {
+  default = "cdcpocdemoproducer1"
+  description   = "App Service name."
+}

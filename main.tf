7ef398e763d66ad44e41b048ea901cdd3dd100eb
Mangesh Belwalkar <belwalkarmangesh@gmail.com>
2022-01-27 19:06:08 +0530

    Added Vnets

--- main.tf
+++ main.tf
@@ -18,3 +18,18 @@ resource "azurerm_resource_group" "rg" {
   name      = var.resource_group_name
   location  = var.resource_group_location
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "cdcpoc-vnet"
+  address_space       = ["10.7.29.0/29"]
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.7.29.0/29"]
+  service_endpoints    = ["Microsoft.Sql"]
+}
